5a35fb72cd58a6da5064a41446382e45d3679ead
CrispyDrone <mybluewaterfall@hotmail.com>
2019-12-04 22:49:50 +0100

    Daily update:
    + Finished the full exercise this time
    + However, it doesn't feel good. I don't feel like I'm thinking properly
    about the refactoring step, nor about the test writing step. Maybe
    partially because the exercise isn't that good since it precludes
    validation of invalid input.

---
+++ StringCalculator.Tests/StringCalculator20191204Tests.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace StringCalculator.Tests
+{
+    [TestClass]
+    public class StringCalculator20191204Tests
+    {
+        [TestMethod]
+        public void Add_EmptyString_ReturnsZero()
+        {
+            var sut = new StringCalculator20191204();
+
+            var result = sut.Add("");
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Add_SingleNumber_ReturnsNumber()
+        {
+            var sut = new StringCalculator20191204();
+
+            var result = sut.Add("9");
+
+            Assert.AreEqual(9, result);
+        }
+
+        [TestMethod]
+        [DataRow("1,9", 10)]
+        [DataRow("1,9,2,10,5", 27)]
+        public void Add_MultipleNumbersSeparatedByCommas_ReturnsSumOfNumbers(string numbers, int expectedSum)
+        {
+            var sut = new StringCalculator20191204();
+
+            var result = sut.Add(numbers);
+
+            Assert.AreEqual(expectedSum, result);
+        }
+
+        [TestMethod]
+        public void Add_NewLineDelimiter_IsAlsoValid()
+        {
+            var sut = new StringCalculator20191204();
+
+            var result = sut.Add("1\n3,5");
+
+            Assert.AreEqual(9, result);
+        }
+
+        [TestMethod]
+        public void Add_DelimiterSpecifiedOnFirstLine_UsesSpecifiedDelimiterOnTopOfDefaultDelimiters()
+        {
+            var sut = new StringCalculator20191204();
+
+            var result = sut.Add("//;\n1;5;10;3,0\n2");
+
+            Assert.AreEqual(21, result);
+        }
+
+        [TestMethod]
+        public void Add_NegativeValues_ThrowsExceptionWithErrorMessageContainingTheInvalidValues()
+        {
+            var sut = new StringCalculator20191204();
+
+            var exception = Assert.ThrowsException<Exception>(() => sut.Add("2,-4,10,-2"));
+
+            Assert.IsTrue(exception.Message.Contains("-4,-2"));
+        }
+
+        [TestMethod]
+        [DataRow("1000,2", 1002)]
+        [DataRow("1001,2", 2)]
+        public void Add_NumbersGreaterThan1000_AreIgnored(string numbers, int expectedSum)
+        {
+            var sut = new StringCalculator20191204();
+
+            var result = sut.Add(numbers);
+
+            Assert.AreEqual(expectedSum, result);
+        }
+
+        [TestMethod]
+        [DataRow("//[;;;]\n1;;;3;;;5,7,0\n12", 28)]
+        [DataRow("//[***]\n1***3***5,7,0\n12", 28)]
+        public void Add_MultiCharacterDelimiters_AreValid(string numbers, int expectedSum)
+        {
+            var sut = new StringCalculator20191204();
+
+            var result = sut.Add(numbers);
+
+            Assert.AreEqual(expectedSum, result);
+        }
+
+        [TestMethod]
+        [DataRow("//[*][%]\n1*2%3", 6)]
+        [DataRow("//[**][%][H]\n1**2%3H5", 11)]
+        public void Add_MultipleDelimitersSpecified_IsValid(string numbers, int expectedSum)
+        {
+            var sut = new StringCalculator20191204();
+
+            var result = sut.Add(numbers);
+
+            Assert.AreEqual(expectedSum, result);
+        }
+    }
+}
---
+++ StringCalculator/StringCalculator20191204.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace StringCalculator
+{
+    public class StringCalculator20191204
+    {
+        public int Add(string numbers)
+        {
+            if (numbers == "") return 0;
+
+            var delimiters = GetAndRemoveDelimiters(ref numbers);
+            var integers = GetIntegers(numbers, delimiters);
+
+            return integers.Sum();
+        }
+
+        private IEnumerable<int> GetIntegers(string numbers, string[] delimiters)
+        {
+            var integers = ParseIntegers(numbers, delimiters);
+            integers = integers.Where(i => i <= 1000);
+            ThrowIfNegatives(integers);
+            return integers;
+        }
+
+        private IEnumerable<int> ParseIntegers(string numbers, string[] delimiters)
+        {
+            var escapedDelimiters = Regex.Escape(string.Join("|", delimiters));
+            var regex = new Regex($@"(-?\d+)(?:{escapedDelimiters})?");
+
+            var match = regex.Match(numbers);
+
+            while (match.Success)
+            {
+                yield return int.Parse(match.Groups[1].Value);
+                match = match.NextMatch();
+            }
+        }
+
+        private void ThrowIfNegatives(IEnumerable<int> integers)
+        {
+            var negatives = integers.Where(i => i < 0);
+            if (negatives.Any())
+                throw new Exception($"Negatives not allowed - {string.Join(",", negatives)}");
+        }
+
+        private string[] GetAndRemoveDelimiters(ref string numbers)
+        {
+            if (numbers.StartsWith("//"))
+            {
+                var indexOfNewLine = numbers.IndexOf('\n');
+                var delimiterLine = numbers.Substring(2, indexOfNewLine - 2);
+                var delimiters = ExtractDelimiters(delimiterLine);
+                numbers = numbers.Substring(indexOfNewLine + 1);
+                return new string[] { ",", "\n"}.Concat(delimiters).ToArray();
+            }
+
+            return new string[] { ",", "\n" };
+        }
+
+        private IEnumerable<string> ExtractDelimiters(string delimiterLine)
+        {
+            var regex = new Regex(@"\[(.+)\]");
+
+            var match = regex.Match(delimiterLine);
+
+            while (match.Success)
+            {
+                yield return match.Groups[1].Value;
+                match = match.NextMatch();
+            }
+        }
+    }
+}
