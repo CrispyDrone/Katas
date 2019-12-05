21e9a178f959e5d4027ed7f0753d008c9a761457
CrispyDrone <mybluewaterfall@hotmail.com>
2019-12-05 22:20:02 +0100

    Daily update
    Did not go very well today, I took longer than 30 minutes to debug an
    issue. I realized yesterday or the day before that that I didn't care
    about delimiters any more because I was just using a regex that would
    grab all the numbers... So today I solved by actually using the
    delimiters.
    
    This assignment isn't very good honestly. If I use the regex approach,
    all tests will pass from the very first one...
    
    I think that tomorrow I will not use regexes any more, and instead use a
    real parsing approach??

---
+++ StringCalculator.Tests/StringCalculator20191205Tests.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator.Tests
+{
+    [TestClass]
+    public class StringCalculator20191205Tests
+    {
+        [TestMethod]
+        public void Add_EmptyString_ReturnsZero()
+        {
+            var sut = new StringCalculator20191205();
+
+            var result = sut.Add("");
+
+            Assert.AreEqual(0, result);
+
+        }
+
+        [TestMethod]
+        public void Add_SingleNumber_ReturnsNumber()
+        {
+            var sut = new StringCalculator20191205();
+
+            var result = sut.Add("9");
+
+            Assert.AreEqual(9, result);
+        }
+
+        [TestMethod]
+        [DataRow("2,8", 10)]
+        [DataRow("1,9,3", 13)]
+        public void Add_TwoNumbersSeparatedByAComma_ReturnsSumOfNumbers(string numbers, int expectedSum)
+        {
+            var sut = new StringCalculator20191205();
+
+            var result = sut.Add(numbers);
+
+            Assert.AreEqual(expectedSum, result);
+        }
+
+        [TestMethod]
+        public void Add_NumbersSeparatedByNewLines_IsAlsoValid()
+        {
+            var sut = new StringCalculator20191205();
+
+            var result = sut.Add("1\n8\n2");
+
+            Assert.AreEqual(11, result);
+        }
+
+        [TestMethod]
+        [DataRow("//;\n9,2", 11)]
+        [DataRow("//*\n103*54", 157)]
+        [DataRow("//_\n13_4", 17)]
+        public void Add_ACustomDelimiterIsSpecifiedOnFirstLine_DelimiterIsUsed(string numbers, int expectedSum)
+        {
+            var sut = new StringCalculator20191205();
+
+            var result = sut.Add(numbers);
+
+            Assert.AreEqual(expectedSum, result);
+        }
+
+        [TestMethod]
+        [DataRow("//;\n-9,2", "-9")]
+        [DataRow("//*\n103*-54,-3", "-54,-3")]
+        [DataRow("//_\n13_4_-39,2,2,-1,-3", "-39,-1,-3")]
+        public void Add_NegativeNumbers_ThrowsExceptionWithMessageContainingTheInvalidNumbers(string numbers, string expectedExceptionMessage)
+        {
+            var sut = new StringCalculator20191205();
+
+            var exception = Assert.ThrowsException<Exception>(() => sut.Add(numbers));
+
+            Assert.IsTrue(exception.Message.Contains(expectedExceptionMessage));
+        }
+
+        [TestMethod]
+        [DataRow("2,1000", 1002)]
+        [DataRow("1,1001", 1)]
+        public void Add_NumbersGreaterThan1000_AreIgnored(string numbers, int expectedSum)
+        {
+            var sut = new StringCalculator20191205();
+
+            var result = sut.Add(numbers);
+
+            Assert.AreEqual(expectedSum, result);
+        }
+
+        [TestMethod]
+        [DataRow("//[;;;]\n2;;;23;;;2", 27)]
+        [DataRow("//[**]\n2**3**1", 6)]
+        [DataRow("//[**]\n2**3,1", 6)]
+        public void Add_CustomMultiCharacterDelimitersSurroundedBySquareBraces_AreValid(string numbers, int expectedSum)
+        {
+            var sut = new StringCalculator20191205();
+
+            var result = sut.Add(numbers);
+
+            Assert.AreEqual(expectedSum, result);
+        }
+
+        [TestMethod]
+        [DataRow("//[;][_]\n2;23_2", 27)]
+        [DataRow("//[*][%]\n2%3*1", 6)]
+        public void Add_MultipleSingleCharacterDelimitersSurroundedBySquareBraces_AreValid(string numbers, int expectedSum)
+        {
+            var sut = new StringCalculator20191205();
+
+            var result = sut.Add(numbers);
+
+            Assert.AreEqual(expectedSum, result);
+        }
+    }
+}
---
+++ StringCalculator/StringCalculator20191205.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class StringCalculator20191205
+    {
+        public int Add(string numbers)
+        {
+            if (numbers == "") return 0;
+
+            var integers = GetIntegers(numbers);
+            ThrowIfNegatives(integers);
+            integers = IgnoreNumbersGreaterThan1000(integers);
+
+            return integers.Sum();
+        }
+
+        private IEnumerable<int> IgnoreNumbersGreaterThan1000(IEnumerable<int> integers)
+        {
+            return integers .Where(i => i <= 1000);
+        }
+
+        private IEnumerable<int> GetIntegers(string numbers)
+        {
+            var delimiters = GetAndRemovetDelimiters(ref numbers);
+
+            return ParseIntegers(numbers, delimiters);
+        }
+
+        private IEnumerable<int> ParseIntegers(string numbers, string[] delimiters)
+        {
+            var escapedDelimiters = delimiters.Select(d => Regex.Escape(d));
+            var regex = new Regex($@"(-?\d+)(?:{string.Join("|", escapedDelimiters)})");
+
+            var match = regex.Match(numbers);
+
+            var lastMatch = match;
+
+            while (match.Success)
+            {
+                yield return int.Parse(match.Groups[1].Value);
+                lastMatch = match;
+                match = match.NextMatch();
+            }
+
+            if (lastMatch.Success)
+            {
+                yield return int.Parse(numbers.Substring(lastMatch.Groups[1].Index + lastMatch.Groups[0].Value.Length));
+            }
+            else
+            {
+                yield return int.Parse(numbers);
+            }
+        }
+
+        private void ThrowIfNegatives(IEnumerable<int> integers)
+        {
+            var negatives = integers.Where(i => i < 0);
+
+            if (negatives.Any())
+                throw new Exception($"Negatives not allowed - {string.Join(",", negatives)}");
+        }
+
+        private string[] GetAndRemovetDelimiters(ref string numbers)
+        {
+            if (numbers.StartsWith("//"))
+            {
+                var customDelimiters = GetCustomDelimiters(numbers.Trim('/'));
+                numbers = RemoveFirstLine(numbers);
+                return new string[] { ",", "\n" }.Concat(customDelimiters).ToArray();
+            }
+
+            return new string[] { ",", "\n" };
+        }
+
+        private string RemoveFirstLine(string numbers)
+        {
+            return numbers.Substring(numbers.IndexOf('\n') + 1);
+        }
+
+        private IEnumerable<string> GetCustomDelimiters(string numbers)
+        {
+            var delimiterLine = GetDelimiterLine(numbers);
+            var regex = new Regex(@"([^\[\]]+)");
+
+            var match = regex.Match(delimiterLine);
+
+            while (match.Success)
+            {
+                yield return match.Groups[1].Value;
+                match = match.NextMatch();
+            }
+        }
+
+        private string GetDelimiterLine(string numbers)
+        {
+            return numbers.Substring(0, numbers.IndexOf('\n'));
+        }
+    }
+}
